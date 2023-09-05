a0f7f9edd4256439730be91c3ece330d2e547b42
Ronny Wang <ronnywang@gmail.com>
2023-09-05 11:30:40 +0800

    add Importer library

---
+++ imports/Importer.php
@@ -0,0 +1,15 @@
+<?php
+
+class Importer
+{
+    public static function getURL($url)
+    {
+        $curl = curl_init($url);
+        // only ipv4
+        curl_setopt($curl, CURLOPT_IPRESOLVE, CURL_IPRESOLVE_V4);
+        // agent chrome
+        curl_setopt($curl, CURLOPT_USERAGENT, 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko');
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+        return curl_exec($curl);
+    }
+}
