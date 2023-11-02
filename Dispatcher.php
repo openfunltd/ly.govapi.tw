6e2a0bb9972baa2212aec54725256933e7f2a027
Ronny Wang <ronnywang@gmail.com>
2023-11-02 15:57:23 +0800

    add /stat API

--- Dispatcher.php
+++ Dispatcher.php
@@ -4,6 +4,7 @@
  * @OA\Info(
  *   title="立法院 API", version="1.0.0"
  * )
+ * @OA\Tag(name="stat", description="統計")
  * @OA\Tag(name="legislator", description="立法委員")
  * @OA\Tag(name="committee", description="委員會")
  * @OA\Tag(name="meet", description="會議")
@@ -1232,6 +1233,168 @@ class Dispatcher
         self::json_output($records);
     }
 
+    /**
+     * @OA\Get(
+     *   path="/stat", summary="取得統計資料", tags={"stat"},
+     *   @OA\Response(response="200", description="統計資料")
+     * )
+     */
+    public static function stat()
+    {
+        $records = new StdClass;
+        // bill
+        $ret = Elastic::dbQuery("/{prefix}bill/_search", 'GET', json_encode([
+            'size' => 0,
+            'aggs' => [
+                'term_count' => [
+                    'terms' => [
+                        'field' => '屆期',
+                        'order' => [ '_key' => 'desc' ],
+                    ],
+                ],
+                'max_mtime' => [
+                    'max' => [
+                        'field' => 'mtime',
+                    ],
+                ],
+            ]
+        ]));
+        $records->bill = new StdClass;
+        $records->bill->total = 0;
+        $records->bill->terms = [];
+        foreach ($ret->aggregations->term_count->buckets as $bucket) {
+            $records->bill->total += $bucket->doc_count;
+            $records->bill->terms[] = [
+                'term' => $bucket->key,
+                'count' => $bucket->doc_count,
+            ];
+        }
+        $records->bill->max_mtime = $ret->aggregations->max_mtime->value;
+        $records->bill->max_mtime_human = date('Y-m-d H:i:s', $records->bill->max_mtime / 1000);
+
+        // legislator
+        $ret = Elastic::dbQuery("/{prefix}legislator/_search", 'GET', json_encode([
+            'size' => 0,
+            'aggs' => [
+                'term_count' => [
+                    'terms' => [
+                        'field' => 'term',
+                        'order' => [ '_key' => 'desc' ],
+                    ],
+                ],
+            ]
+        ]));
+        $records->legislator = new StdClass;
+        $records->legislator->total = 0;
+        $records->legislator->terms = [];
+        foreach ($ret->aggregations->term_count->buckets as $bucket) {
+            $records->legislator->total += $bucket->doc_count;
+            $records->legislator->terms[] = [
+                'term' => $bucket->key,
+                'count' => $bucket->doc_count,
+            ];
+        }
+
+        // gazette
+        $ret = Elastic::dbQuery("/{prefix}gazette/_search", 'GET', json_encode([
+            'size' => 0,
+            'aggs' => [
+                'year_count' => [
+                    'terms' => [
+                        'field' => 'comYear',
+                        'order' => [ '_key' => 'desc' ],
+                    ],
+                ],
+            ],
+        ]));
+        $records->gazette = new StdClass;
+        $records->gazette->total = 0;
+        $records->gazette->agenda_total = 0;
+        $records->gazette->max_meeting_date = 0;
+        $records->gazette->max_meeting_date_human = '';
+        $records->gazette->comYears = [];
+        foreach ($ret->aggregations->year_count->buckets as $bucket) {
+            $records->gazette->total += $bucket->doc_count;
+            $records->gazette->comYears[$bucket->key] = [
+                'year' => $bucket->key,
+                'count' => $bucket->doc_count,
+            ];
+        }
+
+        // gazette_agenda
+        $ret = Elastic::dbQuery("/{prefix}gazette_agenda/_search", 'GET', json_encode([
+            'size' => 0,
+            'aggs' => [
+                'year_count' => [
+                    'terms' => [
+                        'field' => 'comYear',
+                        'order' => [ '_key' => 'desc' ],
+                    ],
+                    'aggs' => [
+                        'max_meeting_date' => [
+                            'max' => [ 'field' => 'meetingDate' ],
+                        ],
+                    ],
+                ],
+            ],
+        ]));
+
+        foreach ($ret->aggregations->year_count->buckets as $bucket) {
+            $records->gazette->agenda_total += $bucket->doc_count;
+            $records->gazette->comYears[$bucket->key]['agenda_count'] = $bucket->doc_count;
+            $records->gazette->comYears[$bucket->key]['max_meeting_date'] = $bucket->max_meeting_date->value;
+            $records->gazette->comYears[$bucket->key]['max_meeting_date_human'] = date('Y-m-d H:i:s', $bucket->max_meeting_date->value / 1000);
+        }
+        $records->gazette->comYears = array_values($records->gazette->comYears);
+        $records->gazette->max_meeting_date = $ret->aggregations->year_count->buckets[0]->max_meeting_date->value;
+        $records->gazette->max_meeting_date_human = date('Y-m-d H:i:s', $records->gazette->max_meeting_date / 1000);
+
+        // meet
+        $ret = Elastic::dbQuery("/{prefix}meet/_search", 'GET', json_encode([
+            'size' => 0,
+            'aggs' => [
+                'term_count' => [
+                    'terms' => [
+                        'field' => 'term',
+                        'order' => [ '_key' => 'desc' ],
+                    ],
+                    'aggs' => [
+                        'max_meeting_date' => [
+                            'max' => [ 'field' => 'meet_data.date'],
+                        ],
+                        'term_meetdata_count' => [
+                            'filter' => [
+                                'exists' => ['field' => 'meet_data.date' ],
+                            ],
+                        ],
+                        'term_議事錄_count' => [
+                            'filter' => [
+                                'exists' => ['field' => '議事錄' ],
+                            ],
+                        ],
+                    ],
+                ],
+            ],
+        ]));
+        $records->meet = new StdClass;
+        $records->meet->total = 0;
+        $records->meet->terms = [];
+        foreach ($ret->aggregations->term_count->buckets as $bucket) {
+            $records->meet->total += $bucket->doc_count;
+            $records->meet->terms[$bucket->key] = [
+                'term' => $bucket->key,
+                'count' => $bucket->doc_count,
+            ];
+            $records->meet->terms[$bucket->key]['max_meeting_date'] = $bucket->max_meeting_date->value;
+            $records->meet->terms[$bucket->key]['max_meeting_date_human'] = date('Y-m-d H:i:s', $bucket->max_meeting_date->value / 1000);
+            $records->meet->terms[$bucket->key]['meetdata_count'] = $bucket->term_meetdata_count->doc_count;
+            $records->meet->terms[$bucket->key]['議事錄_count'] = $bucket->term_議事錄_count->doc_count;
+        }
+        $records->meet->terms = array_values($records->meet->terms);
+        return self::json_output($records);
+
+    }
+
     public static function json_output($obj)
     {
         header('Access-Control-Allow-Origin: *');
@@ -1280,6 +1443,8 @@ class Dispatcher
             self::ivod($terms);
         } else if ('interpellation' == $method) {
             self::interpellation($terms);
+        } else if ('stat' == $method) {
+            self::stat();
         } else {
             header('HTTP/1.0 404 Not Found');
             echo '404 Not Found';
