9fd5dd236da1281bc2381985975c931b5e0e6ebc
Ronny Wang <ronnywang@gmail.com>
2023-11-08 17:27:15 +0800

    避免覆蓋舊資料

--- imports/meet/parse-meet-from-gazette.php
+++ imports/meet/parse-meet-from-gazette.php
@@ -5,6 +5,8 @@ include(__DIR__ . '/../../init.inc.php');
 $start = date('Y');
 for ($y = $start; $y >= 2012; $y --) {
     error_log($y);
+   
+    $meets = [];
     foreach (glob(__DIR__ . sprintf("/../gazette/agenda-txt/LCIDC01_%3d*", $y - 1911)) as $txtfile) {
         preg_match('#LCIDC01_([0-9]+)#', $txtfile, $matches);
         $comYear = intval(substr($matches[1], 0, 3));
@@ -23,29 +25,49 @@ for ($y = $start; $y >= 2012; $y --) {
             } catch (Exception $e) {
                 continue;
             }
-            error_log("parsing $txtfile");
-            $meet = new StdClass;
-            try {
-                $info = GazetteParser::parseAgendaWholeMeetingNote($txtfile, $meet_obj->id);
-            } catch (Exception $e) {
-                continue;
-                throw $e;
-            }
-            $meet->meet_id = $meet_obj->id;
-            $meet->term = $meet_obj->term;
-            $meet->meet_type = $meet_obj->type;
-            $meet->committees = $meet_obj->committees;
-            $meet->sessionPeriod = $meet_obj->sessionPeriod;
-            $meet->sessionTimes = $meet_obj->sessionTimes;
-            $meet->title = str_replace('議事錄', '', $info->title);
-            $meet->{'議事錄'} = $info;
-            unset($info->meet_id);
-            $info->comYear = $comYear;
-            $info->comVolume = $comVolume;
-            $info->comBookId = $comBookId;
-                
-            Elastic::dbBulkInsert('meet', $meet->meet_id, $meet);
+            
+            $meets[$meet_obj->id] = [$meet_obj->id, $txtfile, $meet_obj, $subject, null];
         }
+    }
+    $ret = Elastic::dbQuery('/{prefix}meet/_search', 'GET', json_encode([
+        'query' => ['terms' => ['_id' => array_keys($meets)]],
+        'size' => 1000,
+    ]));
+
+    foreach ($ret->hits->hits as $hit) {
+        $meets[$hit->_id][4] = $hit->_source;
+    }
+
+    foreach ($meets as $meet) {
+        list($meet_id, $txtfile, $meet_obj, $subject, $meet_data) = $meet;
+
+        error_log("parsing $txtfile");
+        if ($meet_data) {
+            $meet = $meet_data;
+        } else {
+            $meet = new StdClass;
+        }
+        try {
+            $info = GazetteParser::parseAgendaWholeMeetingNote($txtfile, $meet_obj->id);
+        } catch (Exception $e) {
+            throw $e;
+        }
+        $meet->meet_id = $meet_obj->id;
+        $meet->term = $meet_obj->term;
+        $meet->meet_type = $meet_obj->type;
+        $meet->committees = $meet_obj->committees;
+        $meet->sessionPeriod = $meet_obj->sessionPeriod;
+        $meet->sessionTimes = $meet_obj->sessionTimes;
+        $meet->title = str_replace('議事錄', '', $info->title);
+        $info->comYear = $comYear;
+        $info->comVolume = $comVolume;
+        $info->comBookId = $comBookId;
+        $meet->{'議事錄'} = $info;
+        unset($info->meet_id);
+        //echo json_encode($meet, JSON_UNESCAPED_UNICODE) . "\n";
+        //continue;
+            
+        Elastic::dbBulkInsert('meet', $meet->meet_id, $meet);
     }
 }
 Elastic::dbBulkCommit();
