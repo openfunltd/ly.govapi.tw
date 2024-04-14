4db563655cf028e4e4e8e75fe1b2468e00848c17
Ronny Wang <ronnywang@gmail.com>
2024-04-14 09:16:58 +0800

    update bill info

--- imports/bill/check-updated-bill.php
+++ imports/bill/check-updated-bill.php
@@ -16,14 +16,26 @@ while ($obj = json_decode(fgets($fp))) {
     }
     //error_log($billNo);
     $content = gzdecode(file_get_contents(__DIR__ . "/bill-html/{$billNo}.gz"));
+    if (strpos($content, '403 Forbidden')) {
+        error_log("{$billNo} 403 Forbidden");
+        unlink(__DIR__ . "/bill-html/{$billNo}.gz");
+        continue;
+    }
     if ($obj->billType == 20 and strpos($content, '委員會發文') and false === strpos($content, '關聯議案')) {
         error_log("{$billNo} 無關聯議案");
         rename(__DIR__ . "/bill-html/{$billNo}.gz", __DIR__ . "/bill-html/old/{$billNo}.gz");
         continue;
     }
-    $values = BillParser::parseBillDetail($billNo, $content);
-    if ($values->{'議案狀態'} != $obj->content2) {
-        error_log("{$values->billNo} {$values->{'議案狀態'}} {$obj->content2}");
+    try {
+        $values = BillParser::parseBillDetail($billNo, $content);
+    } catch (Exception $e) {
+        continue;
+    }
+    $values->{'議案狀態'} = trim(preg_replace('#\(.*\)$#', '', trim($values->{'議案狀態'})));
+    $obj->content5 = trim(preg_replace('#\(.*\)$#', '', $obj->content5));
+    if ($values->{'議案狀態'} != $obj->content5) {
+        error_log(json_encode($obj, JSON_UNESCAPED_UNICODE));
+        error_log("{$values->billNo} html={$values->{'議案狀態'}} list={$obj->content5}");
         rename(__DIR__ . "/bill-html/{$billNo}.gz", __DIR__ . "/bill-html/old/{$billNo}.gz");
     }
 }
