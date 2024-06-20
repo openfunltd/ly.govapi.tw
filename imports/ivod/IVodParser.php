afe61988d686567af246895923efdc46932d9811
Ronny Wang <ronnywang@gmail.com>
2024-06-20 14:24:07 +0800

    add IVodParser::checkMeetFromIVOD

--- imports/ivod/IVodParser.php
+++ imports/ivod/IVodParser.php
@@ -67,4 +67,42 @@ class IVodParser
 
         return $ivod;
     }
+
+    public static function checkMeetFromIVOD($ivod, &$error_name)
+    {
+        if (!preg_match('#^[^「（]*#u', $ivod->{'會議名稱'}, $matches)) {
+            error_log("會議名稱 not found: " . $ivod->{'會議名稱'});
+            readline('continue?');
+            return false;
+        }
+        $name = $matches[0];
+        if (strpos($name, '法院') === 0) {
+            $name = '立' . $name;
+        } elseif ($name == '立法院第10屆第7會期外交及國防委員會第14全體委員會議') {
+            $name = '立法院第10屆第7會期外交及國防委員會第14次全體委員會議';
+        }
+        if (strpos($name, '黨團協商')) {
+            try {
+                $meet_obj = LYLib::consultToId('ivod', $ivod);
+                $ivod->meet = $meet_obj;
+            } catch (Exception $e) {
+            }
+        } elseif (strpos($ivod->{'會議名稱'}, '公聽會')) {
+            // TODO: 處理公聽會
+            //continue;
+        } else {
+            try {
+                //print_r($ivod);
+                $name = str_replace('(變更議程)', '', $name);
+                $meet_obj = LYLib::meetNameToId($name);
+                $ivod->meet = $meet_obj;
+                //print_r($meet_obj);
+            } catch (Exception $e) {
+                error_log(json_encode($ivod, JSON_UNESCAPED_UNICODE));
+                error_log($e->getMessage());
+                $error_name[$name] ++;
+            }
+        }
+        return $ivod;
+    }
 }
--- imports/ivod/import-ivod.php
+++ imports/ivod/import-ivod.php
@@ -26,38 +26,8 @@ foreach ([
             }
         }
 
-        if (!preg_match('#^[^「（]*#u', $ivod->{'會議名稱'}, $matches)) {
-            error_log("會議名稱 not found: " . $ivod->{'會議名稱'});
-            readline('continue?');
+        if (!$ivod = IVodParser::checkMeetFromIVOD($ivod, $error_name)) {
             continue;
-        }
-        $name = $matches[0];
-        if (strpos($name, '法院') === 0) {
-            $name = '立' . $name;
-        } elseif ($name == '立法院第10屆第7會期外交及國防委員會第14全體委員會議') {
-            $name = '立法院第10屆第7會期外交及國防委員會第14次全體委員會議';
-        }
-        if (strpos($name, '黨團協商')) {
-            try {
-                $meet_obj = LYLib::consultToId('ivod', $ivod);
-                $ivod->meet = $meet_obj;
-            } catch (Exception $e) {
-            }
-        } elseif (strpos($ivod->{'會議名稱'}, '公聽會')) {
-            // TODO: 處理公聽會
-            //continue;
-        } else {
-            try {
-                //print_r($ivod);
-                $name = str_replace('(變更議程)', '', $name);
-                $meet_obj = LYLib::meetNameToId($name);
-                $ivod->meet = $meet_obj;
-                //print_r($meet_obj);
-            } catch (Exception $e) {
-                error_log(json_encode($ivod, JSON_UNESCAPED_UNICODE));
-                error_log($e->getMessage());
-                $error_name[$name] ++;
-            }
         }
         $ivod->features = [];
         if (file_exists(__DIR__ . "/ivod-transcript/{$v}.json") and strpos(file_get_contents(__DIR__ . "/ivod-transcript/{$v}.json"), 'status: error') === false) {
