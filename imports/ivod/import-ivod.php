3322815c28ff10bc4103cce39c774f5398c04a06
Ronny Wang <ronnywang@gmail.com>
2024-10-23 17:36:02 +0800

    add ivod import log

--- imports/ivod/import-ivod.php
+++ imports/ivod/import-ivod.php
@@ -2,6 +2,7 @@
 
 include(__DIR__ . '/../../init.inc.php');
 include(__DIR__ . '/IVodParser.php');
+include(__DIR__ . '/../../imports/Importer.php');
 $crawled = 0;
 
 if (!getenv('date')) {
@@ -11,6 +12,7 @@ foreach ([
     'Full' => 'current-full-id',
     'Clip' => 'current-id',
     ] as $type => $v_file) {
+    $overtime_limit = 5; // 有幾個超時的就要替過
     $v = intval(file_get_contents(__DIR__ . '/' . $v_file));
     $error_name = [];
     for (; $v > 0; $v --) {
@@ -25,7 +27,10 @@ foreach ([
         $ivod = IVodParser::parseHTML($v, $content, $type);
         if (getenv('date')) {
             if (strtotime($ivod->start_time) < getenv('date')) {
-                break;
+                $overtime_limit --;
+                if ($overtime_limit <= 0) {
+                    break;
+                }
             }
         }
 
@@ -42,9 +47,9 @@ foreach ([
                     $result['pyannote'] = [];
                     foreach ($obj->pyannote->result->result as $r) {
                         $result['pyannote'][] = [
+                            'speaker' => $r[2],
                             'start' => $r[0],
                             'end' => $r[1],
-                            'speaker' => $r[2],
                         ];
                     }
                 }
@@ -67,8 +72,62 @@ foreach ([
             $ivod->gazette = json_decode(file_get_contents($gazette_file));
             $ivod->features[] = 'gazette';
         }
+
+        $ivod_target_file = __DIR__ . "/ivod-data/{$v}.json";
+        if (!file_exists($ivod_target_file)) {
+            file_put_contents($ivod_target_file, json_encode($ivod, JSON_UNESCAPED_UNICODE));
+            Elastic::dbBulkInsert('ivod', $ivod->id, $ivod);
+
+            Importer::addImportLog([
+                'event' => 'ivod-add',
+                'group' => 'ivod',
+                'message' => sprintf("增加 ivod: %s", $ivod->id),
+                'data' => json_encode([
+                    'ivod_id' => $ivod->id,
+                    '委員名稱' => $ivod->{'委員名稱'},
+                    'meet_id' => $ivod->meet->id,
+                    '會議名稱' => $ivod->{'會議名稱'},
+                    'type' => $type,
+                    'features' => $ivod->features,
+                ], JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES),
+            ], $commit = false);
+            continue;
+        }
+        $old_ivod_data = json_decode(file_get_contents($ivod_target_file));
+        if (json_encode($ivod) == json_encode($old_ivod_data)) {
+            continue;
+        }
+
+        $changes = [];
+        if (!property_exists($ivod, 'gazette') and !property_exists($old_ivod_data, 'gazette')) {
+            // do nothing
+        } else if (json_encode($ivod->gazette) != json_encode($old_ivod_data->gazette)) {
+            $changes[] = 'gazette';
+        }
+
+        if (!property_exists($ivod, 'transcript') and !property_exists($old_ivod_data, 'transcript')) {
+            // do nothing
+        } else if (json_encode($ivod->transcript) != json_encode($old_ivod_data->transcript)) {
+            $changes[] = 'transcript';
+        }
+        Importer::addImportLog([
+            'event' => 'ivod-change',
+            'group' => 'ivod',
+            'message' => sprintf("變更 ivod: %s (%s)", $ivod->id, implode(',', $changes)),
+            'data' => json_encode([
+                'ivod_id' => $ivod->id,
+                '委員名稱' => $ivod->{'委員名稱'},
+                'meet_id' => $ivod->meet->id,
+                '會議名稱' => $ivod->{'會議名稱'},
+                'type' => $type,
+                'features' => $ivod->features,
+                'changes' => $changes,
+            ], JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES),
+        ], $commit = false);
+
+        file_put_contents($ivod_target_file, json_encode($ivod, JSON_UNESCAPED_UNICODE));
         Elastic::dbBulkInsert('ivod', $ivod->id, $ivod);
     }
 }
 Elastic::dbBulkCommit();
-print_r(array_keys($error_name));
+Importer::addImportLog(null, $commit = true);
