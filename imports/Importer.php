621d87612485ba34d01c2ba658aa0613b126bf0d
Ronny Wang <ronnywang@gmail.com>
2024-10-23 15:02:47 +0800

    add Importer::postToSlack

--- imports/Importer.php
+++ imports/Importer.php
@@ -42,4 +42,24 @@ class Importer
             Elastic::dbBulkCommit();
         }
     }
+
+    public static function postToSlack($slack_url, $message)
+    {
+        $data = [
+            'text' => $message,
+        ];
+        $data_string = json_encode($data);
+        $ch = curl_init($slack_url);
+        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'POST');
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $data_string);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, [
+            'Content-Type: application/json',
+            'Content-Length: ' . strlen($data_string),
+        ]);
+        $result = curl_exec($ch);
+        curl_close($ch);
+        return $result;
+    }
+
 }
