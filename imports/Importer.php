42eaa6d263f77e411d48a259664bd86cc2c8e18f
Ronny Wang <ronnywang@gmail.com>
2024-10-23 14:10:13 +0800

    add Importer::getURL() $retry and $timeout config

--- imports/Importer.php
+++ imports/Importer.php
@@ -2,15 +2,34 @@
 
 class Importer
 {
-    public static function getURL($url)
+    public static function getURL($url, $timeout = 10, $retry_max = 3)
     {
-        $curl = curl_init($url);
-        // only ipv4
-        curl_setopt($curl, CURLOPT_IPRESOLVE, CURL_IPRESOLVE_V4);
-        // agent chrome
-        curl_setopt($curl, CURLOPT_USERAGENT, 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko');
-        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-        return curl_exec($curl);
+        for ($retry = 0; $retry < $retry_max; $retry++) {
+            $curl = curl_init($url);
+            // only ipv4
+            curl_setopt($curl, CURLOPT_IPRESOLVE, CURL_IPRESOLVE_V4);
+            curl_setopt($curl, CURLOPT_TIMEOUT, $timeout);
+            // agent chrome
+            curl_setopt($curl, CURLOPT_USERAGENT, 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko');
+            curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+            $content = curl_exec($curl);
+            $info = curl_getinfo($curl);
+            $error = curl_error($curl);
+            // if timeout,  continue
+            if ($info['http_code'] == 200) {
+                curl_close($curl);
+                return $content;
+            }
+            error_log(sprintf("Failed to get URL: %s retry: %d error:(%d) %s",
+                $url,
+                $retry,
+                $info['http_code'],
+                $error));
+            error_log('Failed to get URL: ' . $url . ' retry: ' . $retry . ' error: ' . $error);
+            sleep($retry + 1);
+            curl_close($curl);
+        }
+        throw new Exception('Failed to get URL: ' . $url);
     }
 
     public static function addImportLog($data, $commit = true)
