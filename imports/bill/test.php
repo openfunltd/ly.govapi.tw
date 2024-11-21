e4dbc62bcb39fc2d2afc64b53dfe75d86c6b0468
Ronny Wang <ronnywang@gmail.com>
2024-11-21 15:14:21 +0800

    update bill test script

--- imports/bill/test.php
+++ imports/bill/test.php
@@ -6,10 +6,6 @@ $billNo = $_SERVER['argv'][1];
 if (!file_exists(__DIR__ . "/bill-html/{$billNo}.gz")) {
     throw new Exception("{$billNo} html not exists");
 }
-if (!file_Exists(__DIR__ . '/output.jsonl')) {
-    throw new Exception("output.jsonl not exists");
-}
-$obj = json_decode(`grep $billNo output.jsonl`);
 $content = gzdecode(file_get_contents(__DIR__ . "/bill-html/{$billNo}.gz"));
 try {
     $values = BillParser::parseBillDetail($billNo, $content);
@@ -18,23 +14,39 @@ try {
 }
 $types = BillParser::getBillTypes();
 $sources = BillParser::getBillSources();
-$values->{'議案類別'} = $types[$obj->billType];
-$values->{'提案來源'} = $sources[$obj->proposalType];
 $values = BillParser::addBillInfo($values);
 echo json_encode($values, JSON_UNESCAPED_UNICODE) . "\n";
 
+$obj = null;
+$doc_file = __DIR__ . "/bill-docgz/{$billNo}.doc.gz";
 if (file_exists(__DIR__ . '/bill-doc-parsed/tikahtml/' . $billNo . '.doc.gz')) {
     $file = __DIR__ . '/bill-doc-parsed/tikahtml/' . $billNo . '.doc.gz';
-    error_log($file);
     $content = gzdecode(file_get_contents($file));
 } else if (file_exists(__DIR__ . '/bill-doc-parsed/html/' . $billNo . '.doc.gz')) {
     $file = __DIR__ . '/bill-doc-parsed/html/' . $billNo . '.doc.gz';
     $obj = json_decode(gzdecode(file_get_contents($file)));
     $content = (base64_decode($obj->content));
+} else if (file_exists($doc_file) and filesize($doc_file) == 5 and file_get_contents($doc_file) == 'array') {
+    $content = '';
+    foreach (glob(__DIR__ . "/bill-doc-parsed/tikahtml/{$billNo}-*.doc.gz") as $file) {
+        error_log($file);
+        $html = gzdecode(file_get_contents($file));
+        if ($content) {
+            $pos = strpos($content, '</body>');
+            $content = substr($content, 0, $pos);
+
+            $pos = strpos($html, '<body>');
+            $html = substr($html, $pos + 6);
+            $content .= $html;
+        } else {
+            $content = $html;
+        }
+    }
+    file_put_contents('tmp-content.html', $content);
 } else {
     exit;
 }
 echo json_encode($values, JSON_UNESCAPED_UNICODE) . "\n";
 echo "=======\n";
 $docdata = BillParser::parseBillDoc($billNo, $content, $obj);
-echo json_encode($docdata, JSON_UNESCAPED_UNICODE) . "\n";
+echo json_encode($docdata, JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT) . "\n";
