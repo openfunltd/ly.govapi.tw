763f650bb7b464b8bbf5640d54c75ed1b5f2f379
Ronny Wang <ronnywang@gmail.com>
2026-06-10 12:04:42 +0800

    增加 timeout 判斷

--- imports/ivod/crawl-html.php
+++ imports/ivod/crawl-html.php
@@ -44,6 +44,8 @@ for ($v = max(4609, $current_id) - 20; ; $v ++) {
         $curl = curl_init($url);
         curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
         curl_setopt($curl, CURLOPT_USERAGENT, 'Mozilla/5.0 (Windows NT 6.1; rv:8.0) Gecko/20100101 Firefox/8.0');
+        // timeout 30 seconds
+        curl_setopt($curl, CURLOPT_TIMEOUT, 30);
         // ipv4 only
         curl_setopt($curl, CURLOPT_IPRESOLVE, CURL_IPRESOLVE_V4);
         $content = curl_exec($curl);
@@ -65,6 +67,7 @@ for ($v = max(4609, $current_id) - 20; ; $v ++) {
 }
 
 $current_id = intval(file_get_contents(__DIR__ . '/current-id'));
+$error = 0;
 for ($v = max($current_id, 146300); ; $v ++) {
     $url = sprintf("https://ivod.ly.gov.tw/Play/Clip/1M/%d", $v);
     $html_target = __DIR__ . "/html/{$v}.html";
@@ -75,13 +78,21 @@ for ($v = max($current_id, 146300); ; $v ++) {
     $curl = curl_init($url);
     curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
     curl_setopt($curl, CURLOPT_USERAGENT, 'Mozilla/5.0 (Windows NT 6.1; rv:8.0) Gecko/20100101 Firefox/8.0');
+    // timeout 30 seconds
+    curl_setopt($curl, CURLOPT_TIMEOUT, 30);
     // ipv4 only
     curl_setopt($curl, CURLOPT_IPRESOLVE, CURL_IPRESOLVE_V4);
     $content = curl_exec($curl);
     if (!preg_match('#readyPlayer\("([^"]*)"#', $content, $matches)) {
         error_log("readyPlayer not found {$url}");
-        break;
+        $error ++;
+        if ($error > 10) {
+            error_log("too many errors, stop");
+            break;
+        }
+        continue;
     }
+    $error = 0;
     if ($v > $current_id) {
         file_put_contents(__DIR__ . '/current-id', $v);
     }
